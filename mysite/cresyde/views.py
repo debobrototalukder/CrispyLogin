1fed59e8f245c47e18c744844c8df030e7a1a065
debobrototalukder <debobrototalukder@gmail.com>
2019-07-31 16:15:20 +0400

    Final_working_Complete_V2

--- mysite/cresyde/views.py
+++ mysite/cresyde/views.py
@@ -8,32 +8,19 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import ProfileForm, ProfileExtraForm, LoginForm
+from .forms import ProfileForm, ProfileExtraForm
 
 
 class Registration(generic.CreateView):
     form_class = UserCreationForm
-    #success_url = reverse_lazy('login')
     template_name = 'register.html'
 
     def get(self, request, *args):
         return render(request, self.template_name, {'form': ProfileForm, 'form2': ProfileExtraForm})
 
-    def post(self, request):
+    def post(self, request, **kwargs):
         form = ProfileForm(request.POST)
         form2 = ProfileExtraForm(request.POST)
-        #
-        # if form.is_valid():
-        #     user = form.save()
-        #     print('words')
-        #     print(form2)
-        #
-        #     if form2.is_valid():
-        #         print("something")
-        #         values = form2.save(commit=False)
-        #         values.fKey = user
-        #         form2.save()
-
         if all((form.is_valid(), form2.is_valid())):
             user = form.save()
             values = form2.save(commit=False)
